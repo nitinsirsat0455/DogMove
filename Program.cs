2902778e250b10fe71801e9c39c5718f07f05166
Nitin Sirsat <nitinsirsat.it@gmail.com>
2021-12-10 01:03:18 +0530

    DogMoveNew

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace DogMove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogPosition1 = (@"
+           __
+      (___()'`;
+      /,,,,,/`
+      \\   \\");
+
+
+
+
+            string dogPosition2 = (@"
+             __
+        )___()'`;
+        /,,,,,/`
+       //   //");
+
+            for(int i=0;i<100;i++)
+            {
+                Console.WriteLine(dogPosition1);
+                Thread.Sleep(1000);
+                Console.Clear();
+
+                Console.WriteLine(dogPosition2);
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+        }
+    }
+}
